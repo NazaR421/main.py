8330e0537fdd45c17a33ab9892fdaafbfa244a82
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2023-10-16 18:19:04 +0300

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,25 +1,29 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication,QWidget, QPushButton,QSpinBox, QLabel, QVBoxLayout,QRadioButton,QHBoxLayout,QButtonGroup,QGroupBox
 
-card_width, card_height=600,500
-app=QApplication([])
-win_card= QWidget()
-win_card.rasize(card_width,card_height)
+
+card_width,card_height=600,500
+win_card=QWidget()
+win_card.resize(card_width,card_height)
 win_card.move(300,300)
 win_card.setWindowTitle("Memory Card")
 
-btn_Sleep = QPushButton("Відпочити")
-box_Minutes=QSpinBox()
-box_Minutes.setValue(30)
 
-window=QWidget()
+
 
 btn_menu=QPushButton("Меню")
 btn_rest=QPushButton("Відпочити")
-btn_nex=QPushButton("Відповисти")
-sp_rest=QSpinBox()
+btn_next=QPushButton("Відповісти")
+box_Minutes=QSpinBox()
+box_Minutes.setValue(30)
 
-RadioGroupBox=QGroupBox("Варіант відповідей")
+
+
+
+RadioGroupBox=QGroupBox("Варіанти відповідей")
+
+lb_Result=QLabel("")
+lb_Correct=QLabel("")
 
 RadioGroup=QButtonGroup()
 
@@ -27,11 +31,11 @@ rbtn_ans1=QRadioButton("1")
 rbtn_ans2=QRadioButton("2")
 rbtn_ans3=QRadioButton("3")
 rbtn_ans4=QRadioButton("4")
-
 RadioGroup.addButton(rbtn_ans1)
 RadioGroup.addButton(rbtn_ans2)
 RadioGroup.addButton(rbtn_ans3)
 RadioGroup.addButton(rbtn_ans4)
+
 
 layout_ans1=QHBoxLayout()
 layout_ans2=QVBoxLayout()
@@ -45,5 +49,31 @@ layout_ans3.addWidget(rbtn_ans4)
 layout_ans1.addLayout(layout_ans2)
 layout_ans1.addLayout(layout_ans3)
 RadioGroupBox.setLayout(layout_ans1)
+
+layout_ans4=QVBoxLayout()
+layout_ans5=QHBoxLayout()
+layout_ans6=QHBoxLayout()
+layout_ans7=QHBoxLayout()
+layout_ans8=QHBoxLayout()
+
+layout_ans5.addWidget(btn_menu)
+layout_ans5.addStretch(1)
+layout_ans5.addWidget(btn_rest)
+layout_ans5.addWidget(box_Minutes)
+
+layout_ans6.addWidget(lb_Result,alignment=(Qt.AlignmentFlag.AlignCenter))
+
+layout_ans7.addWidget(RadioGroupBox)
+
+layout_ans8.addStretch(1)
+layout_ans8.addWidget(btn_next,stretch=2)
+layout_ans8.addStretch(1)
+
+layout_ans4.addLayout(layout_ans5, stretch=1)
+layout_ans4.addLayout(layout_ans6,stretch=2)
+layout_ans4.addLayout(layout_ans7,stretch=1)
+layout_ans4.addLayout(layout_ans8,stretch=2)
+
+
 
 win_card.show()
