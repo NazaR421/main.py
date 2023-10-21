a341fca0b353f58343c2287a8cd8ac1862411f2a
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2023-10-21 16:19:14 +0300

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QWidget, QPushButton,QSpinBox, QLabel, QVBoxLayout,QRadioButton,QHBoxLayout,QButtonGroup,QGroupBox
+from PyQt5.QtWidgets import QWidget, QPushButton,QSpinBox, QLabel, QVBoxLayout,QRadioButton,QHBoxLayout,QButtonGroup,QGroupBox,QApplication
 
+app=QApplication([])
 
 card_width,card_height=600,500
 win_card=QWidget()
@@ -48,7 +49,14 @@ layout_ans3.addWidget(rbtn_ans4)
 
 layout_ans1.addLayout(layout_ans2)
 layout_ans1.addLayout(layout_ans3)
+
 RadioGroupBox.setLayout(layout_ans1)
+
+layout_res=QVBoxLayout()
+layout_res.addWidget(lb_Result,alignment=( Qt.AlignLeft Qt.AlignTop))
+layout_res.addWidget(lb_Correct,alignment=Qt.AlignHCenter,stretch=2)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
 
 layout_ans4=QVBoxLayout()
 layout_ans5=QHBoxLayout()
@@ -72,8 +80,34 @@ layout_ans8.addStretch(1)
 layout_ans4.addLayout(layout_ans5, stretch=1)
 layout_ans4.addLayout(layout_ans6,stretch=2)
 layout_ans4.addLayout(layout_ans7,stretch=1)
+layout_ans4.addStretch(1)
 layout_ans4.addLayout(layout_ans8,stretch=2)
+layout_ans4.addStretch(1)
+layout_ans4.setSpacing(5)
+
+win_card.setLayout(layout_ans4)
+
+
+
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_next.setText("Наступне питання")
+
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_next.setText('Відповісти')
+    RadioGroup.setExclusive(False)
+    rbtn_ans1.setChecked(False)
+    rbtn_ans2.setChecked(False)
+    rbtn_ans3.setChecked(False)
+    rbtn_ans4.setChecked(False)
+    RadioGroup.setExclusive(True)
+
 
 
 
 win_card.show()
+app.exec()
